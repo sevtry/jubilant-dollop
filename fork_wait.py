37a4e84d310e3932b7caac7e7fff441a4c33979b
sevtry <122721667+sevtry@users.noreply.github.com>
2023-01-15 01:48:47 -0800

    Update fork_wait.py

--- fork_wait.py
+++ fork_wait.py
@@ -1,7 +1,7 @@
 """This test case provides support for checking forking and wait behavior.
 #
 To test different wait behavior, override the wait_impl method.
-#
+
 We want fork1() semantics -- only the forking thread survives in the
 child after a fork().
 #
